741f78457f82e3e94a32188dcf3db5411ecd6b69
Tomas Sokac <tomas.sokac@unitedclassifieds.sk>
2022-11-02 11:12:43 +0100

    add my account

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -1,9 +1,39 @@
+import { PlusIcon, QueueListIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
 import { NextPageWithLayout } from "../_app";
 
 const Account: NextPageWithLayout = () => {
     return (
-        <div>
-            Moje konto
+        <div className="w-full lg:w-4/5 py-2 px-5 lg:p-0 space-y-4">
+            <div className='flex items-center justify-between'>
+                <div className="text-2xl font-semibold">Moje konto</div>
+                <div>
+                    <button className="button-primary">
+                        Pridať do dražby
+                    </button>
+                </div>
+            </div>
+            <div className="grid md:grid-cols-6 gap-3 lg:gap-4 lg:grid-cols-4">
+                <div className="col-span-2 lg:col-span-1">
+                    <ul className='space-y-2 w-full'>
+                        <li className="group">
+                            <Link href={"/"} className="text-gray-700 flex items-center space-x-4 border bg-gray-100 py-1 px-4 rounded-lg group-hover:text-black group-hover:border-gray-300">
+                                <QueueListIcon className="w-5 h-5" />
+                                <span>Moje dražby</span>
+                            </Link>
+                        </li>
+                        <li className="group">
+                            <Link href={"/"} className="text-gray-700 flex items-center space-x-4 border bg-gray-100 py-1 px-4 rounded-lg group-hover:text-black group-hover:border-gray-300">
+                                <PlusIcon className="w-5 h-5" />
+                                <span>Pridať do dražby</span>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className='md:col-span-4 lg:col-span-3'>
+                    obsah
+                </div>
+            </div>
         </div>
     )
 }
