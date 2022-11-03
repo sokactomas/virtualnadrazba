8a6625f968c7f33f67895e45c1128cd9cec5b67d
Tomas Sokac <tomas.sokac@unitedclassifieds.sk>
2022-11-03 21:25:55 +0100

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
 import '../../public/globals.css'
 import { NextPage } from "next";
-import type { AppProps } from "next/app";
+import type { AppProps, AppType } from "next/app";
 import { ReactElement, ReactNode } from "react";
 import { Layout } from "components/Layout";
 import { getSession, SessionProvider } from "next-auth/react";
 import { trpc } from 'utils/trpc';
+import { Session } from 'next-auth';
 
 export type NextPageWithLayout<TProps = Record<string, unknown>, TInitialProps = TProps> = NextPage<TProps, TInitialProps> & {
     getLayout?: (page: ReactElement) => ReactNode
@@ -12,16 +13,17 @@ export type NextPageWithLayout<TProps = Record<string, unknown>, TInitialProps =
 
 type AppPropsWithLayout = AppProps & {
     Component: NextPageWithLayout
+    pageProps: { auth?: boolean; session?: Session }
 }
 
 const App = (({ Component, pageProps }: AppPropsWithLayout) => {
     const getLayout = Component.getLayout ?? ((page) => <Layout>{page}</Layout>)
     return (
-        <SessionProvider session={pageProps['session']} refetchInterval={5 * 60}>
+        <SessionProvider session={pageProps?.session} refetchInterval={5 * 60}>
             { getLayout(<Component {...pageProps} />) }
         </SessionProvider>
     ) 
-}) as any;
+}) as AppType;
 
 App.getInitialProps = async ({ ctx }: { ctx: any }) => {
     return {
