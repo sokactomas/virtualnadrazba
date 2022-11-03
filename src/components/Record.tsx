38d72fa8e4b3ba4509689242b904ececbe474fc4
Tomas Sokac <tomas.sokac@unitedclassifieds.sk>
2022-11-03 16:14:34 +0100

    add pkgs

--- src/components/Record.tsx
+++ src/components/Record.tsx
@@ -1,4 +1,4 @@
-import {  ClockIcon, EyeIcon, FireIcon, PhotoIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import {  ArchiveBoxIcon, ClockIcon, EyeIcon, FireIcon, PhotoIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import { TagIcon } from "@heroicons/react/24/solid";
 import { useDuration } from "hooks/duration";
 import moment, { Duration } from "moment";
@@ -80,6 +80,34 @@ export const Record: FC<RecordProps> = ({ record }) => {
         return null;
     }
 
+    const renderPkgLabel = () => {
+        let label: string;
+        switch(record?.pkg) {
+            case 0:
+                label = 'C2C'
+                break;
+            case 1:
+                label = 'C2B'
+                break;
+            case 2:
+                label = 'Combo'
+                break;
+            default:
+                label = 'Neuvedené'
+                break;
+        }
+        return label;
+    }
+
+    const renderPkg = () => {
+        return (
+            <div className="absolute bottom-0 justify-center left-0 right-0 font-bold bg-green-700/80 z-10 text-white py-1 px-4 flex items-center space-x-2">
+                <ArchiveBoxIcon className="w-4 h-4" />
+                <span>{ renderPkgLabel() }</span>
+            </div>
+        )
+    }
+
     const renderImage = () => {
         if (record?.image) {
             return <Image src={record?.image?.previewUrls?.thumbnail || ''} alt={record?.title} layout='fill' objectFit='cover' />
@@ -111,6 +139,7 @@ export const Record: FC<RecordProps> = ({ record }) => {
         <div className='flex flex-col w-full bg-white border-2 border-gray-200'>
             <div className="flex p-4">
                 <div className="relative flex-[192px] shrink-0 grow-0 h-[146px] flex items-center justify-center">
+                    { renderPkg() }
                     {renderImage()}
                 </div>
                 <div className="px-2 w-full flex flex-col items-start justify-between">
