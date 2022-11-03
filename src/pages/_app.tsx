bf9e68534917fddab181f55de9e89161ad2ddf3a
Tomas Sokac <tomas.sokac@unitedclassifieds.sk>
2022-11-03 21:07:17 +0100

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -23,7 +23,7 @@ const App = (({ Component, pageProps: { session, ...pageProps} }: AppPropsWithLa
     ) 
 }) as AppType;
 
-App.getInitialProps = async ({ ctx }) => {
+App.getInitialProps = async ({ ctx }: { ctx: any }) => {
     return {
         session: await getSession(ctx)
     }
